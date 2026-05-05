f7636a1ad0a21c2ba6a531a0b7ad9e6f66cf607a
Subhrodeep Acharya <subhrodeepacharya14@gmail.com>
2026-05-05 17:24:03 +0530

    backend api added

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: '/api'||import.meta.env.VITE_API_URL,
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
 });
